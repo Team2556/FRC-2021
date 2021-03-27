a143356a96f7d04e9300c915af98a8dfaf6931dc
somnomly <brnzbrwn@gmail.com>
2021-03-27 16:35:45 -0500

    fixed shooter class probably added temp control and some other cool stuff a lot of comments

--- src/main/include/Subsystems/Shooter.h
+++ src/main/include/Subsystems/Shooter.h
@@ -9,49 +9,60 @@
 
 #include "Utilities/Debug.h"
 
-#define MAX_SHOOT_SPEED_ERROR 100 // since the shooter is spinning at about 11,000rpm, the error can be this high while still being close enough
-
 class Shooter
 {
 public:
   Shooter(Robot *pRobot);
 
-  // Wheel Functions
-  bool SpinUp(); //uses encoder units per second spins up motor until reaches SetSpinSpeed (Running RPM)
+  //Wheel (Shooter) Functions
   void SetSpinSpeed(double setSpinSpeed); //sets the target speed of motor (Desired RPM)
-  void StopSpin();
-  bool TargetSpeed(); //checks if motor is at target speed
+  bool TargetSpeed();                     //checks if motor is at target speed
+  bool SpinUp();                          //runs motor to SetSpinSpeed (Running RPM)
+  void StopSpin();                        //stops shooter motors
 
-  //Hood
-  double SetHood(double setHoodAngle /*units is encoder ticks*/); //sets hood angle
-  bool HoodAimed(); //checks angle of hood
-  bool MoveHood(double setHoodSpeed); //Move hood to the angle SetHood has at given speed
+  //Hood Functions
+  void SetHood(double setHoodAngle); //sets hood angle target
+  bool HoodAimed();                  //checks angle of hood
+  bool MoveHood();                   //Move hood to the angle SetHood has
+
+  //Manual Functions
+  //these set the speed and hood angle to any input (such as pulled from a joystick) for testing purposes and the like
+  bool ManualShoot(double manualSetSpinSpeed);
+  bool ManualHood(double manualHoodAngle);
 
   Robot *pRobot;
 
-  // All of these values need to be set in the .cpp file
-	double setShooterSpeedValue = pRobot->DriverCMD->shooterSpeedMult * maxEncoderSpeed; //need a function to set shooter speed; this is a placeholder for that
-  int maxHoodAngle = -1200; //placeholder number for max hood angle
-							              //(need to ask mechanical for real number)
-	double setHoodValue = pRobot->DriverCMD->hoodAngleMult * maxHoodAngle; //setHoodAngle = 75% of max hood angle
-                                                                        //need a function to set hood angle; this is a placeholder for that
-  double setHoodSpeed; //need a function to set hood speed; this is a placeholder for that
+  //Variables
 
+  //All of these values need to be set in the .cpp file
+  double setShooterSpeedValue;
+  double setHoodValue;
 
+  //can define these in drivebase or something, possibly connect them to a joystick
+  double manualSetSpinSpeed;
+  double manualHoodAngle;
+
+  //Other
   Debug ShooterDebug{"/Subsystems/Shooter"};
 
 private:
-  //CHECK ROBOT MAP (In Utilities) FOR MOTOR NAMES
-  //Define two falcon 500s here. These will be the motors spinning the wheel that actually shoots the balls
-
+  //Shooter Motors
   WPI_TalonFX Shooter_Motor_1{SHOOTER_MOTOR_1};
   WPI_TalonFX Shooter_Motor_2{SHOOTER_MOTOR_2};
 
-  //Define a talon SRX here. This will move the hood
-
+  //Hood Motor
   WPI_TalonSRX Hood_Motor{HOOD_MOTOR};
 
+  //Solenoid
+  frc::DoubleSolenoid Shooter_Solenoid{CAN_PCM_SHOOTER, SHOOTER_OUT, SHOOTER_IN};
 
-  int maxEncoderSpeed = 2048;
-	//Built-In Encoder Feedback: 2048 CPR Mag Encoder
+  //Error Values
+#define MAX_SHOOT_SPEED_ERROR 100 //since the shooter is spinning at about 11,000rpm, the error can be this high while still being close enough
+#define MAX_HOOD_ANGLE_ERROR 5
+
+  //Max and Min Values
+#define MAX_TEMPERATURE 50 //in Celcius
+#define MAX_ENCODER_SPEED 2048
+#define MAX_HOOD_ANGLE 1200
+#define MIN_HOOD_ANGLE 0
 };
